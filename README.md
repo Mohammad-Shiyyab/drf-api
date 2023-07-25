9e4d4f4263dcee495d30ee3fa0045c1204850840
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-07-25 06:22:57 +0000

    docker

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # drf-api
+
+# LAB - Class 31
+
+## Project: Django REST Framework & Docker
+
+## Author: Mohammad Al Shiyab
+
+## Setup
+
+you need to have docker installed on your machine
+
+docker compose up
+
+## How to initialize/run your application
+
+docker compose run web python manage.py migrate
+
+docker compose run web python manage.py createsuperuser
+
+## Tests
+python manage.py 
--- things/views.py
+++ things/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Thing
+from rest_framework.generics import ListAPIView , RetrieveAPIView , ListCreateAPIView
+from .serializers import ThingSerializer
 
+from .serializers import ThingSerializer
 # Create your views here.
+
+
+class ThingListView(ListCreateAPIView):
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+
+class ThingDetailView(RetrieveAPIView):  
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer  
